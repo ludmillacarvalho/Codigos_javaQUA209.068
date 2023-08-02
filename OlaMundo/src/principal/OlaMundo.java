ad2cedc31fb5a51caa509cd6e768f8b4590c3d2f
Ludmilla Carvalho <ludmilla.dba@gmail.com>
2023-08-02 17:29:50 -0300

    aula 02/08/23

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class OlaMundo {
+	//metodo principal, onde executa o programa
+
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova linha
+		//print = imprimir; In = new line;
+		
+		System.out.println("Hello world, Olá Mundo!");
+ 
+		// comentario de uma linha
+		
+		/*esse é um contario de multiplas linhas */
+	}
+
+}
